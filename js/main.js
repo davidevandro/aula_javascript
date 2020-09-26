708ee4ca773ebde9810463efc9353d1e399c5ba6
David Martins <davidevandro@gmail.com>
2020-09-25 21:16:22 -0300

    Criando estrutura basica de HTML e Javascript com funcoes de mensagem

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var nome = "David Martins";
+var idade = 32;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+
+//Escreve na janela de pop-up
+//alert("meu primeiro js")
+
+//Concatenção de strings
+//alert("Bem vindo "+ nome)
+//alert(nome + " tem " + idade + " anos.");
+
+//Soma de inteiros
+//alert(idade + idade2)
+
+//Escreve no console
+//console.log(nome);
+//console.log(idade + idade2)
+
+//Replace na string
+//console.log(frase.replace("Japão", "Brasil"))
+
+//Uppercase
+//console.log(frase.toUpperCase())
+//Lowercase
+//console.log(frase.toLowerCase())
