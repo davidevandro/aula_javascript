0aea21a00b534364e3ab98e0b6d1c4fa1241f742
David Martins <davidevandro@gmail.com>
2020-09-25 22:21:17 -0300

    Criando Funções em Javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 
+//-------- AULA 04 - Funções
+//function soma(n1, n2){
+//    return n1 + n2;
+//}
 
+//alert(soma(5,10));
+
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    } else{
+        validar = false;
+    }
+    return validar;
+}*/
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+//function setReplace(frase, nome, novo_nome){
+//    return frase.replace(nome, novo_nome);
+//}
+
+//alert(setReplace("Vai Japão!", "Japão", "Brasil"))
 //------------AULA 03 - Datas
 //var d = new Date();
 //alert(d);
