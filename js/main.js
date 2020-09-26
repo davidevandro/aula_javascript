bbae1cc873503f382cef59d4beb1041c25906226
David Martins <davidevandro@gmail.com>
2020-09-25 21:24:39 -0300

    Criando Javascript com funções de Array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var nome = "David Martins";
-var idade = 32;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo"
+//------------------AULA 02 - Array
+//var lista = ["maça", "pera", "laranja"];
+//Adiciona item na lista
+//lista.push("uva");
+//Remove o ultimo item da lista
+//lista.pop()
+//Imprime a lista
+//console.log(lista);
+//Array indexado a partir de 0
+//console.log(lista[1]);
+//Tamanho da lista
+//console.log(lista.length);
+//Inverte a lista
+//console.log(lista.reverse());
+//Converte em string
+//console.log(lista.toString())
+//Mudar a forma de converter a lista para string
+//console.log(lista.join(" - "))
+
+//----------------AULA 01 - Mensagens + Strings
+//var nome = "David Martins";
+//var idade = 32;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
 
 //Escreve na janela de pop-up
 //alert("meu primeiro js")
